816442b732acf08132a0e68758315b197cdbc754
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2024-03-08 20:51:34 -0500

    continuo realizando operaciones para calcular el porcentaje de comision en cada venta y comparando para encontrar cual es la mayor, presento inconvenientes con una de las variables

--- PARCIAL #1/Program.cs
+++ PARCIAL #1/Program.cs
@@ -8,7 +8,8 @@ class Program
         double comisiones = 0.10;
         int meta = 1000000;
         double venta1,venta2,venta3;
-       
+        double comisiones1, comisiones2, comisiones3;
+        double comisionalta, mayorventa;
 
         Console.WriteLine("***Venta #1***");
         Console.WriteLine("Escribir el valor en $ de la venta 1:");
@@ -21,5 +22,28 @@ class Program
         Console.WriteLine("***Venta #3***");
         Console.WriteLine("Escribir el valor en $ de la venta 3:");
         venta3 = Convert.ToDouble(Console.ReadLine);
+
+        comisiones1 = venta1 * comisiones;
+        comisiones2 = venta2 * comisiones;
+        comisiones3 = venta3 * comisiones;
+
+
+        if (comisiones1>comisionalta)
+        {
+            comisionalta = comisiones1;
+            mayorventa = venta1;
+        }
+        if (comisiones2>comisionalta)
+        {
+            comisionalta = comisiones2;
+            mayorventa = venta2;
+        }
+        if (comisiones3 > comisionalta)
+        {
+            comisionalta = comisiones3;
+            mayorventa = venta3;
+        }
+        double comisionestotales = comisiones1 + comisiones2 + comisiones3;
+
     }
 }
