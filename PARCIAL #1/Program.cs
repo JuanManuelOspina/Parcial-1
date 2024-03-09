60f4ed069067b2378b8417e3d6b771881aa98628
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2024-03-08 21:02:10 -0500

    se valida error de la variable involucrada en el condicional, y la única solución que encuentro es inicializarla asignandole un cero. luego calculo los promedios para darle el dato al usuario

--- PARCIAL #1/Program.cs
+++ PARCIAL #1/Program.cs
@@ -7,9 +7,10 @@ class Program
         double salariobase = 650000;
         double comisiones = 0.10;
         int meta = 1000000;
-        double venta1,venta2,venta3;
+        double venta1, venta2, venta3;
         double comisiones1, comisiones2, comisiones3;
         double comisionalta, mayorventa;
+        double comisionestotales,medianacomisiones,ventas;
 
         Console.WriteLine("***Venta #1***");
         Console.WriteLine("Escribir el valor en $ de la venta 1:");
@@ -27,13 +28,14 @@ class Program
         comisiones2 = venta2 * comisiones;
         comisiones3 = venta3 * comisiones;
 
+        comisionalta = 0;
 
-        if (comisiones1>comisionalta)
+        if (comisiones1 > comisionalta)
         {
             comisionalta = comisiones1;
             mayorventa = venta1;
         }
-        if (comisiones2>comisionalta)
+        if (comisiones2 > comisionalta)
         {
             comisionalta = comisiones2;
             mayorventa = venta2;
@@ -43,7 +45,11 @@ class Program
             comisionalta = comisiones3;
             mayorventa = venta3;
         }
-        double comisionestotales = comisiones1 + comisiones2 + comisiones3;
+        comisionestotales = comisiones1 + comisiones2 + comisiones3;
+        comisionestotales = salariobase + comisionestotales;
+        medianacomisiones = comisionestotales / 3;
+
+        ventas = venta1 + venta2 + venta3;
 
     }
 }
