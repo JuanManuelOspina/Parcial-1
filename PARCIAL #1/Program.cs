523ae44ae0933c6cd52a729e3ca529e708ce65be
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2024-03-08 20:40:20 -0500

    se procede a pedir los valores de las ventas para poder comparar y surgen nuevas variables, así que se declaran

--- PARCIAL #1/Program.cs
+++ PARCIAL #1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salariobase = 650000;
+        double comisiones = 0.10;
+        int meta = 1000000;
+        double venta1,venta2,venta3;
+       
+
+        Console.WriteLine("***Venta #1***");
+        Console.WriteLine("Escribir el valor en $ de la venta 1:");
+        venta1 = Convert.ToDouble(Console.ReadLine);
+
+        Console.WriteLine("***Venta #2***");
+        Console.WriteLine("Escribir el valor en $ de la venta 2:");
+        venta2 = Convert.ToDouble(Console.ReadLine);
+
+        Console.WriteLine("***Venta #3***");
+        Console.WriteLine("Escribir el valor en $ de la venta 3:");
+        venta3 = Convert.ToDouble(Console.ReadLine);
+    }
+}
