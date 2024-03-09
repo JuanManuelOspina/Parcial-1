da06fa51522372e9a49500d8eb578d828888ad98
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2024-03-08 22:03:54 -0500

    por error de git hub no me permitió realizar el commit, me generaba error, aqui logro realizar el commit para que pueda quedar guardado

---
+++ PARCIAL #1/punto2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Timers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salidatren1 = 10.0;
+        double velotren1 = 80.0;
+        double salidatren2=11.0;
+        double velotren2=100.0;
+        double distanciatren1, alcanzartren1, hora;
+
+        distanciatren1 = velotren1 * (salidatren2 - salidatren1);
+        alcanzartren1 = distanciatren1 / (velotren2 - velotren1);
+        hora = salidatren2 + alcanzartren1;
+
+        Console.WriteLine("El tren alcanzará al otro a las "+ hora.ToString("0.00")+"horas");
+    }
+}
